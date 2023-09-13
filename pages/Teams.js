44d6897a43ca3fb133dba8ca6a77469dea28766f
Ieee-Nfsu <144695465+Ieee-Nfsu@users.noreply.github.com>
2023-09-13 21:21:37 +0000

    uploaded images and increased sizes

--- pages/Teams.js
+++ pages/Teams.js
@@ -45,64 +45,66 @@ var Program_team=[
   "Linkdin":"www.linkedin.com/in/kunal-choudhary-23aa81224",
   "github":"https://github.com/kunal0x00",
   "mail":"mailto:kunalchoudhary@ieee.org",
-  "profile_url":""
+  "profile_url":"https://lh3.googleusercontent.com/d/1Xdx9w4Zp6pNhbNRxgJCb-WCkxezIYD4j"
 },
-    {
+  {
   "Name":"Supriy Rathi",
   "Linkdin":"",
   "github":"",
   "mail":"",
-  "profile_url":""
+  "profile_url":"https://lh3.googleusercontent.com/d/1U7gDwDKQVmy-w8C1SiJAk0T-BrVbghya"
 },
   {
   "Name":"Krishita Choksi",
   "Linkdin":"",
   "github":"",
   "mail":"",
-  "profile_url":""
+  "profile_url":"https://lh3.googleusercontent.com/d/"
 },
   {
   "Name":"Varshila Tamboli",
   "Linkdin":"",
   "github":"",
   "mail":"",
-"profile_url":""
+"profile_url":"https://lh3.googleusercontent.com/d/17DX4pPixhmmlv17j_Pv4eHYdQ6WuQuqP"
 },
   {
   "Name":"Taneesha Shah",
   "Linkdin":"http://linkedin.com/in/taneesha-shah-b7356b236",
   "github":"http://github.com/tan0001",
   "mail":"mailto:taneesha.shah@ieee.org",
-  "profile_url":""
+  "profile_url":"https://lh3.googleusercontent.com/d/14v_u6VouXTxUiT1nzrMiiVM3fa-kn6rD"
 },
   {
   "Name":"Soumitri Barury",
   "Linkdin":"",
   "github":"https://github.com/SoumitriB",
   "mail":"mailto:soumitri@ieee.org",
-  "profile_url":""
+  "profile_url":"https://lh3.googleusercontent.com/d/1f9vlFfgqrQLvjLeb9he0bGJ8yVLWAGrZ"
 },
+  
   {
   "Name":"Abhishek Kanzariya",
   "Linkdin":"",
   "github":"",
   "mail":"",
-  "profile_url":""
+  "profile_url":"https://lh3.googleusercontent.com/d/1Pp0xsfzd1kym2nhV9y4qytnjpj0qbzxU"
 },
   {
   "Name":"Johana Sallangi",
   "Linkdin":"",
   "github":"",
   "mail":"",
-  "profile_url":""
+  "profile_url":"https://lh3.googleusercontent.com/d/1__gqShCnQfnVuV2LWcFdtYEDYQDVWtBH"
 },
   {
   "Name":"Belinda Grace",
   "Linkdin":"",
   "github":"",
   "mail":"",
-  "profile_url":""
+  "profile_url":"https://lh3.googleusercontent.com/d/1_YsqChD4bOf1mBl_VxLN9B9ZsYGLKjiz"
 },];
+    
   var finance_team=[
       {
   "Name":"Khushi Bansal",
@@ -117,7 +119,7 @@ var Program_team=[
   "Linkdin":"",
   "github":"",
   "mail":"",
-  "profile_url":""
+  "profile_url":"https://lh3.googleusercontent.com/d/1RC8yBWxSxljmc_tAnJtDXorr3S3C6Xdo"
 },
 ];
   var media_team=[
@@ -126,14 +128,14 @@ var Program_team=[
   "Linkdin":"https://www.linkedin.com/in/nitish-deshpande-a095341ba/",
   "github":"https://github.com/nitishdeshpande07",
   "mail":"mailto:nitishdeshpande07@ieee.org",
-  "profile_url":""
+  "profile_url":"https://lh3.googleusercontent.com/d/1RNOJ7_U7XwRAh5DPr5-nEm3bgYzuOfwF"
 },
     {
   "Name":"Achal Patil",
   "Linkdin":"",
   "github":"",
   "mail":"",
-  "profile_url":""
+  "profile_url":"https://lh3.googleusercontent.com/d/"
 },];
 var membership_team=[
 
@@ -142,15 +144,23 @@ var membership_team=[
   "Linkdin":"https://www.linkedin.com/in/nitish-deshpande-a095341ba/",
   "github":"https://github.com/nitishdeshpande07",
   "mail":"mailto:nitishdeshpande07@ieee.org",
-  "profile_url":""
+  "profile_url":"https://lh3.googleusercontent.com/d/1RNOJ7_U7XwRAh5DPr5-nEm3bgYzuOfwF"
 },
     {
   "Name":"Sneh singh",
   "Linkdin":"",
   "github":"",
   "mail":"",
-  "profile_url":""
-} 
+  "profile_url":"https://lh3.googleusercontent.com/d/1XmKie6NQlGWxnOS1kQj6Ta3DsggHH31b"
+},
+
+   {
+  "Name":"Johana Sallangi",
+  "Linkdin":"",
+  "github":"",
+  "mail":"",
+  "profile_url":"https://lh3.googleusercontent.com/d/1__gqShCnQfnVuV2LWcFdtYEDYQDVWtBH"
+}
 ];
 
   
@@ -159,8 +169,8 @@ const style = {
   short_title:"text-4xl text-[#006ba1] font-bold text-center mt-4",
   cardscontainer:"mx-4 mt-16 gap-16 flex flex-wrap items-center md:justify-around justify-center",
   single_cardcontainer:" text-white w-[300px] flex flex-col items-center shadow-black hover:shadow-xl bg-slate-800 rounded-2xl overflow-hidden ",
-  imgcontainer:" flex flex-col justify-center items-center rounded-b-full p-8 bg-[#9ca3af] ",
-  img:"h-32 w-32 rounded-full ",
+  imgcontainer:" flex flex-col justify-center items-center rounded-b-full p-6 bg-[#9ca3af] ",
+  img:"h-36 w-36 rounded-full ",
   pos:"text-center pt-2 text-xl",
   name:"text-3xl",
   description:"",
@@ -312,7 +322,33 @@ export default function Teams()
   })}
        </div>
       </div>
+
     
+     {/*Membership Team*/}
+     <div>
+        <h1 className={style.short_title}>Membership Committee</h1>
+        <hr className="h-3 text-gray-700"/>
+         <div className={`${style.cardscontainer} ${style.centerLastTwo}`}>
+     {membership_team.map((member)=>{
+      return(<>
+    <div className={style.single_cardcontainer}>
+      <div className={style.imgcontainer}>
+        <img className={style.img} src={member.profile_url}/>
+        <p className={style.pos}>{member.Position}</p>
+      </div>
+      {/* <hr style={{color:"#f00",height:"5px"}}/> */}
+      <div className={style.name}>{member.Name}</div>
+      <div className={style.linkcontainer}>
+        <a href={member.Linkdin}>Linekdin</a>
+        <a href={member.github}>Github</a>
+        <a href={member.mail}>Mail</a>
+      </div>
+     </div>
+   
+      </>);
+  })}
+       </div>
+      </div>
     
     <Footer/>
   </div>
